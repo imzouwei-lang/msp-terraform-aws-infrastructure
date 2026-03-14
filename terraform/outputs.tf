7f478dab5c74b8ac427e7cbcb9afc422ea4280f1
imzouwei-lang <imzou.wei@gmail.com>
2026-03-14 17:40:29 +0800

    Only S3 bucket output

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,13 +1,3 @@
-output "msp_02_instance_id" {
-  description = "MSP-02 实例 ID"
-  value       = aws_instance.msp_02.id
-}
-
-output "msp_02_public_ip" {
-  description = "MSP-02 公网 IP"
-  value       = aws_instance.msp_02.public_ip
-}
-
 output "s3_bucket_name" {
   description = "S3 存储桶名称"
   value       = aws_s3_bucket.msp_storage.bucket
