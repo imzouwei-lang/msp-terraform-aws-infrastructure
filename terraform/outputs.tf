05d28a301721ec0815ffb744babc3d799b19d37e
imzouwei-lang <imzou.wei@gmail.com>
2026-03-14 16:23:47 +0800

    Update outputs for msp-02

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,29 +1,9 @@
-output "monitoring_instance_id" {
-  description = "监控服务器实例 ID"
-  value       = aws_instance.monitoring.id
+output "msp_02_instance_id" {
+  description = "MSP-02 实例 ID"
+  value       = aws_instance.msp_02.id
 }
 
-output "monitoring_public_ip" {
-  description = "监控服务器公网 IP"
-  value       = aws_instance.monitoring.public_ip
-}
-
-output "wiki_instance_id" {
-  description = "Wiki 服务器实例 ID"
-  value       = aws_instance.wiki.id
-}
-
-output "wiki_public_ip" {
-  description = "Wiki 服务器公网 IP"
-  value       = aws_instance.wiki.public_ip
-}
-
-output "grafana_url" {
-  description = "Grafana 访问地址"
-  value       = "http://${aws_instance.monitoring.public_ip}:3000"
-}
-
-output "wiki_url" {
-  description = "Wiki 访问地址"
-  value       = "http://${aws_instance.wiki.public_ip}"
+output "msp_02_public_ip" {
+  description = "MSP-02 公网 IP"
+  value       = aws_instance.msp_02.public_ip
 }
