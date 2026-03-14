b9d884b581358f6839aabd7e5d15988f1382c21e
imzouwei-lang <admin@iplayable.com>
2026-03-14 12:34:26 +0000

    feat: only create msp01 EC2 instance

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,19 +1,9 @@
-output "s3_bucket_1_name" {
-  description = "S3 bucket 1 name"
-  value       = aws_s3_bucket.msp_bucket_1.id
+output "msp01_instance_id" {
+  description = "MSP01 instance ID"
+  value       = aws_instance.msp01.id
 }
 
-output "s3_bucket_1_arn" {
-  description = "S3 bucket 1 ARN"
-  value       = aws_s3_bucket.msp_bucket_1.arn
-}
-
-output "s3_bucket_2_name" {
-  description = "S3 bucket 2 name"
-  value       = aws_s3_bucket.msp_bucket_2.id
-}
-
-output "s3_bucket_2_arn" {
-  description = "S3 bucket 2 ARN"
-  value       = aws_s3_bucket.msp_bucket_2.arn
+output "msp01_public_ip" {
+  description = "MSP01 public IP"
+  value       = aws_instance.msp01.public_ip
 }
