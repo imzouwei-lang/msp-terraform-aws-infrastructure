f9eea7feece804c9c18a232288ac5d8235ca8ef0
imzouwei-lang <imzou.wei@gmail.com>
2026-03-14 20:00:22 +0800

    Update outputs for msp01

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,9 +1,9 @@
-output "msp_02_instance_id" {
-  description = "MSP-02 实例 ID"
-  value       = aws_instance.msp_02.id
+output "msp01_instance_id" {
+  description = "msp01 实例 ID"
+  value       = aws_instance.msp01.id
 }
 
-output "msp_02_public_ip" {
-  description = "MSP-02 公网 IP"
-  value       = aws_instance.msp_02.public_ip
+output "msp01_public_ip" {
+  description = "msp01 公网 IP"
+  value       = aws_instance.msp01.public_ip
 }
