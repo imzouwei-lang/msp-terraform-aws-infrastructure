0f64755522776c842c29db1caf26a76379373c7a
imzouwei-lang <admin@iplayable.com>
2026-03-14 09:05:12 +0000

    Add S3 storage bucket

--- terraform/resources.tf
+++ terraform/resources.tf
@@ -98,6 +98,19 @@ resource "aws_instance" "msp_02" {
     Name = "msp-02"
   }
 }
-# Trigger deploy
-# Redeploy trigger
-# Deploy msp-02 instance
+
+# S3 存储桶
+resource "aws_s3_bucket" "msp_storage" {
+  bucket = "msp-storage-288761743095"
+
+  tags = {
+    Name = "msp-storage"
+  }
+}
+
+resource "aws_s3_bucket_versioning" "msp_storage" {
+  bucket = aws_s3_bucket.msp_storage.id
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
