6ffe76b1ab699d00290a4560914e0beb8a1ebcd4
imzouwei-lang <imzou.wei@gmail.com>
2026-03-14 17:23:32 +0800

    Update outputs for msp-02 EC2

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,4 +1,9 @@
-output "s3_bucket_name" {
-  description = "S3 存储桶名称"
-  value       = aws_s3_bucket.msp_storage.bucket
+output "msp_02_instance_id" {
+  description = "MSP-02 实例 ID"
+  value       = aws_instance.msp_02.id
+}
+
+output "msp_02_public_ip" {
+  description = "MSP-02 公网 IP"
+  value       = aws_instance.msp_02.public_ip
 }
