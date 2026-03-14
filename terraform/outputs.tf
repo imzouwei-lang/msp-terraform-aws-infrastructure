310f4bd2cc901267b44c79642fd4073ee956c366
imzouwei-lang <admin@iplayable.com>
2026-03-14 11:17:47 +0000

    Remove msp-02 EC2 server

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,13 +1,3 @@
-output "msp_02_instance_id" {
-  description = "MSP-02 实例 ID"
-  value       = aws_instance.msp_02.id
-}
-
-output "msp_02_public_ip" {
-  description = "MSP-02 公网 IP"
-  value       = aws_instance.msp_02.public_ip
-}
-
 output "msp_03_instance_id" {
   description = "MSP-03 实例 ID"
   value       = aws_instance.msp_03.id
