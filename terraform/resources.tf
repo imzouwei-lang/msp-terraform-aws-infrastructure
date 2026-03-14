0eca0548e87f42ff1007413ec3ee7bf32c046b74
imzouwei-lang <admin@iplayable.com>
2026-03-14 12:43:21 +0000

    feat: delete S3 buckets

--- terraform/resources.tf
+++ terraform/resources.tf
@@ -1,15 +1,1 @@
-resource "aws_s3_bucket" "msp_bucket_1" {
-  bucket = "msp-storage-1-${var.environment}"
-
-  tags = {
-    Name = "msp-storage-1-${var.environment}"
-  }
-}
-
-resource "aws_s3_bucket" "msp_bucket_2" {
-  bucket = "msp-storage-2-${var.environment}"
-
-  tags = {
-    Name = "msp-storage-2-${var.environment}"
-  }
-}
+# No resources - S3 buckets removed
