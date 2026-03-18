8d6196fa2a45ebb4dcfa795e9d1fe0c0e69d6b57
imzouwei-lang <imzou.wei@gmail.com>
2026-03-18 23:27:47 +0800

    Remove S3 outputs

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,9 +1,1 @@
-output "s3_bucket_1_name" {
-  description = "S3 存储桶 1 名称"
-  value       = aws_s3_bucket.msp_bucket_1.id
-}
-
-output "s3_bucket_2_name" {
-  description = "S3 存储桶 2 名称"
-  value       = aws_s3_bucket.msp_bucket_2.id
-}
+# No outputs
