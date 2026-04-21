19f4e9ae50e47c0cd0ee6151234cdc4a00e1faf7
imzouwei-lang <admin@iplayable.com>
2026-04-21 11:49:04 +0000

    feat: create single EC2 instance msp-06 with security group

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,1 +1,9 @@
-# No outputs
+output "msp_06_public_ip" {
+  description = "MSP-06 server public IP"
+  value       = aws_instance.msp_06.public_ip
+}
+
+output "msp_06_instance_id" {
+  description = "MSP-06 server instance ID"
+  value       = aws_instance.msp_06.id
+}
